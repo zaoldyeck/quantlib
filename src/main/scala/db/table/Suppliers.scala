5af1f257065ba80e551c718939a8970a7abd1b6e
zaoldyeck <zaoldyeck9970@gmail.com>
2019-11-04 00:41:50 +0800

    Add sample table Suppliers

---
+++ src/main/scala/db/table/Suppliers.scala
@@ -0,0 +1,14 @@
+package db.table
+
+import slick.jdbc.H2Profile.api._
+
+class Suppliers(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
+  def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
+  def name = column[String]("SUP_NAME")
+  def street = column[String]("STREET")
+  def city = column[String]("CITY")
+  def state = column[String]("STATE")
+  def zip = column[String]("ZIP")
+  // Every table needs a * projection with the same type as the table's type parameter
+  def * = (id, name, street, city, state, zip)
+}
