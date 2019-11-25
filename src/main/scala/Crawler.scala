f200f5d0df4852f5455c8cc9428cdddd8c2e310a
zaoldyeck <zaoldyeck9970@gmail.com>
2019-11-25 19:13:48 +0800

    Add get financial analysis function

--- src/main/scala/Crawler.scala
+++ src/main/scala/Crawler.scala
@@ -9,6 +9,56 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 
 class Crawler {
+  def getFinancialAnalysis(year: Int): Future[File] = {
+    year - 1911 match {
+      case y if y > 100 =>
+      // After IFRS
+
+      case y if y > 100 && y < 104 =>
+      // Both
+      case y if y < 104 =>
+        // Before IFRS
+        val formData = Map(
+          "encodeURIComponent" -> Seq(1),
+          "step" -> Seq(1),
+          "firstin" -> Seq(1),
+          "off" -> Seq(1),
+          "TYPEK" -> Seq("sii"),
+          "year" -> Seq(80))
+        Http.client.url("https://mops.twse.com.tw/mops/web/ajax_t51sb02").post(formData).flatMap {
+          res =>
+            val fd = Map(
+              "firstin" -> Seq(true),
+              "step" -> Seq(10),
+              "filename" -> Seq(s"${y}_b.csv"))
+            Http.client.url("https://mops.twse.com.tw/server-java/t105sb02").withCookies(res.cookies).withBody(fd).withMethod("POST").withRequestTimeout(5.minutes).stream()
+        } flatMap {
+          res =>
+            val fileName = res.header("Content-disposition").get.split("filename=")(1).replace("\"", "")
+            val file = new File(s"../data/quarterly_report/$fileName")
+            file.getParentFile.mkdirs()
+            val outputStream = java.nio.file.Files.newOutputStream(file.toPath)
+
+            // The sink that writes to the output stream
+            val sink = Sink.foreach[ByteString] { bytes =>
+              outputStream.write(bytes.toArray)
+            }
+
+            // materialize and run the stream
+            res.bodyAsSource
+              .runWith(sink)
+              .andThen {
+                case result =>
+                  // Close the output stream whether there was an error or not
+                  outputStream.close()
+                  // Get the result or rethrow the error
+                  result.get
+              }
+              .map(_ => file)
+        }
+    }
+  }
+
   def getMonthlyReport = {
 
   }
