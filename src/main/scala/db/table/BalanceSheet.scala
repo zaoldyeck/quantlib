4d45f4764bf5bb5884775f72b9b0f17a18be8967
zaoldyeck <zaoldyeck9970@gmail.com>
2019-11-28 01:15:10 +0800

    Add BalanceSheet and StatementOfComprehensiveIncome class with info

---
+++ src/main/scala/db/table/BalanceSheet.scala
@@ -0,0 +1,13 @@
+package db.table
+
+/**
+ * https://mops.twse.com.tw/mops/web/t163sb05
+ * https://emops.twse.com.tw/server-java/t58query#3
+ * Schema https://emops.twse.com.tw/server-java/t163sb05_e?step=show&year=2019&season=3
+ * 資產負債表
+ *
+ * @param tag
+ */
+class BalanceSheet {
+
+}
---
+++ src/main/scala/db/table/StatementOfComprehensiveIncome.scala
@@ -0,0 +1,13 @@
+package db.table
+
+/**
+ * https://mops.twse.com.tw/mops/web/t163sb04
+ * https://emops.twse.com.tw/server-java/t58query#3
+ * Schema https://emops.twse.com.tw/server-java/t163sb04_e?step=show&year=2019&season=3
+ * 綜合損益表
+ *
+ * @param tag
+ */
+class StatementOfComprehensiveIncome {
+
+}
