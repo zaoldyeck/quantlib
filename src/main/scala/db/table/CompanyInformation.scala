06f63bc67a8f101fc009fa7881d4b5e46dd306ef
zaoldyeck <zaoldyeck9970@gmail.com>
2019-11-28 19:49:06 +0800

    Add table class company information

---
+++ src/main/scala/db/table/CompanyInformation.scala
@@ -0,0 +1,13 @@
+package db.table
+
+/**
+ * https://mops.twse.com.tw/mops/web/t51sb01
+ * https://emops.twse.com.tw/server-java/t58query#1
+ * Schema https://emops.twse.com.tw/server-java/t51sb01_e
+ * 公司基本資料
+ *
+ * @param tag
+ */
+class CompanyInformation {
+
+}
