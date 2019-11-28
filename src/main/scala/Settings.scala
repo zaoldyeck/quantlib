f4c7048bf3d54f67683f710bbd369f2d8547a878
zaoldyeck <zaoldyeck9970@gmail.com>
2019-11-28 19:48:19 +0800

    Edit operating revenue before IFRSs settings

--- src/main/scala/Settings.scala
+++ src/main/scala/Settings.scala
@@ -5,14 +5,23 @@ object Settings {
   private val conf: Config = ConfigFactory.load
 
   object financialAnalysis {
+    val dir: String = conf.getString("data.financialAnalysis.dir")
     val page: String = conf.getString("data.financialAnalysis.page")
     val file: String = conf.getString("data.financialAnalysis.file")
-    val dir: String = conf.getString("data.financialAnalysis.dir")
   }
 
   object operatingRevenue {
-    val file: String = conf.getString("data.operatingRevenue.file")
     val dir: String = conf.getString("data.operatingRevenue.dir")
+
+    object beforeIFRSs {
+      val page: String = conf.getString("data.financialAnalysis.beforeIFRSs.page")
+      val file: String = conf.getString("data.financialAnalysis.beforeIFRSs.file")
+    }
+
+    object afterIFRSs {
+      val file: String = conf.getString("data.operatingRevenue.afterIFRSs.file")
+    }
+
   }
 
   val quarterlyReportDir: String = conf.getString("data.quarterlyReport.dir")
