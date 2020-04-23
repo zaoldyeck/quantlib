db8afd8c73c5a97902db6acd817e4f9785463869
zaoldyeck <zaoldyeck9970@gmail.com>
2020-04-24 01:08:33 +0800

    Beautify comment

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,7 +12,7 @@ object Main {
   // 月營收(102/1 後) https://mops.twse.com.tw/nas/t21/sii/t21sc03_102_1_0.html
   // 除權息(92/5/5 後) https://www.twse.com.tw/exchangeReport/TWT49U?response=csv&strDate=20190701&endDate=20190719
   // 除權息(92/5/5 後) https://www.twse.com.tw/zh/page/trading/exchange/TWT48U.html
-  // 減資(100/1/1 後)https://www.twse.com.tw/zh/page/trading/exchange/TWTAUU.html
+  // 減資(100/1/1 後) https://www.twse.com.tw/zh/page/trading/exchange/TWTAUU.html
   // 減資(100/1/1 後) https://www.twse.com.tw/exchangeReport/TWTAUU?response=csv&strDate=20190701&endDate=20190714
   // 每日成交價(93/2/11 後) https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=20190718&type=ALL
   // 每日本益比、殖利率及股價淨值比(94/9/2 後) https://www.twse.com.tw/zh/page/trading/exchange/BWIBBU_d.html
