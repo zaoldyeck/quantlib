8edcb06a06066ea38b36682eceb766e546d93955
zaoldyeck <zaoldyeck9970@gmail.com>
2020-05-16 19:10:41 +0800

    Add ETFs list to settings

--- src/main/scala/Settings.scala
+++ src/main/scala/Settings.scala
@@ -3,6 +3,7 @@ import com.typesafe.config.ConfigFactory
 
 object Settings {
   private val conf: Config = ConfigFactory.load
+  val ETFs = Set("0050", "0051", "0052", "0053", "0054", "0055", "0056", "0057", "0058", "0059", "006203", "006204", "006208", "00690", "00692", "00701", "00713", "00730", "00728", "00731", "00733", "00742", "006201")
 
   object financialAnalysis {
     val page: String = conf.getString("data.financialAnalysis.page")
