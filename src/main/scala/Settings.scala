68c7ec6b5360545b0bbbed20540044a670002c19
zaoldyeck <zaoldyeck9970@gmail.com>
2020-06-25 12:17:25 +0800

    Debug wrong data source url

--- src/main/scala/Settings.scala
+++ src/main/scala/Settings.scala
@@ -109,9 +109,10 @@ object Settings {
     val twse: Detail = new TwseAfterIFRSsDetail
     val tpex: Detail = new TpexAfterIFRSsDetail
     val markets: Seq[Detail] = year match {
+      case y if y < 1993 => Seq(new TwseBeforeIFRSsDetail)
       case y if y < 2012 => Seq(new TwseBeforeIFRSsDetail, new TpexBeforeIFRSsDetail)
-      case y if y > 2011 && y < 2015 => Seq(new TwseBeforeIFRSsDetail, new TwseAfterIFRSsDetail, new TpexBeforeIFRSsDetail, new TpexAfterIFRSsDetail)
-      case _ => Seq(new TwseAfterIFRSsDetail, new TpexAfterIFRSsDetail)
+      case y if y > 2014 => Seq(new TwseAfterIFRSsDetail, new TpexAfterIFRSsDetail)
+      case _ => Seq(new TwseBeforeIFRSsDetail, new TwseAfterIFRSsDetail, new TpexBeforeIFRSsDetail, new TpexAfterIFRSsDetail)
     }
   }
 
@@ -149,12 +150,97 @@ object Settings {
 
   val quarterlyReportDir: String = conf.getString("data.quarterlyReport.dir")
 
-  object statementOfComprehensiveIncome {
-    val page: String = conf.getString("data.statementOfComprehensiveIncome.page")
-    val file: String = conf.getString("data.statementOfComprehensiveIncome.file")
-    val dir: String = conf.getString("data.statementOfComprehensiveIncome.dir")
+  case class BalanceSheetSetting(year: Int, season: Int) extends Setting {
+
+    class TwseBeforeIFRSsDetail extends TwseDetail(LocalDate.of(1989, 1, 1), None, LocalDate.of(year, season, 1)) {
+      private val y = super.endDate.getYear - 1911
+      val file: String = conf.getString("data.balanceSheet.file")
+      val dir: String = conf.getString("data.balanceSheet.dir.twse")
+      override val page: String = conf.getString("data.balanceSheet.page.beforeIFRSs")
+      override val url: String = file
+      override val fileName = s"${super.endDate.getYear}_${super.endDate.getMonthValue}_b_"
+
+      override def formData = Map(
+        "encodeURIComponent" -> "1",
+        "step" -> "1",
+        "firstin" -> "1",
+        "off" -> "1",
+        "isQuery" -> "Y",
+        "TYPEK" -> "sii",
+        "year" -> y.toString,
+        "season" -> s"0${super.endDate.getMonthValue}")
+    }
+
+    class TwseAfterIFRSsDetail extends TwseBeforeIFRSsDetail {
+      override val page: String = conf.getString("data.balanceSheet.page.afterIFRSs")
+      override val fileName = s"${super.endDate.getYear}_${super.endDate.getMonthValue}_a_"
+    }
+
+    class TpexBeforeIFRSsDetail extends TwseBeforeIFRSsDetail {
+      override val dir: String = conf.getString("data.balanceSheet.dir.tpex")
+
+      override def formData: Map[String, String] = super.formData + ("TYPEK" -> "otc")
+    }
+
+    class TpexAfterIFRSsDetail extends TpexBeforeIFRSsDetail {
+      override val page: String = conf.getString("data.balanceSheet.page.afterIFRSs")
+      override val fileName = s"${super.endDate.getYear}_${super.endDate.getMonthValue}_a_"
+    }
+
+    val twse: Detail = new TwseAfterIFRSsDetail
+    val tpex: Detail = new TpexAfterIFRSsDetail
+    val markets: Seq[Detail] = year match {
+      case y if y < 1993 => Seq(new TwseBeforeIFRSsDetail)
+      case y if y < 2013 => Seq(new TwseBeforeIFRSsDetail, new TpexBeforeIFRSsDetail)
+      case _ => Seq(new TwseAfterIFRSsDetail, new TpexAfterIFRSsDetail)
+    }
   }
 
+  case class IncomeStatementSetting(year: Int, quarter: Int) extends Setting {
+
+    class TwseBeforeIFRSsDetail extends TwseDetail(LocalDate.of(1989, 1, 1), None, LocalDate.of(year, quarter, 1)) {
+      private val y = super.endDate.getYear - 1911
+      val file: String = conf.getString("data.incomeStatement.file")
+      val dir: String = conf.getString("data.incomeStatement.dir.twse")
+      override val page: String = conf.getString("data.incomeStatement.page.beforeIFRSs")
+      override val url: String = file
+      override val fileName = s"${super.endDate.getYear}_${super.endDate.getMonthValue}_b_"
+
+      override def formData = Map(
+        "encodeURIComponent" -> "1",
+        "step" -> "1",
+        "firstin" -> "1",
+        "off" -> "1",
+        "isQuery" -> "Y",
+        "TYPEK" -> "sii",
+        "year" -> y.toString,
+        "season" -> s"0${super.endDate.getMonthValue}")
+    }
+
+    class TwseAfterIFRSsDetail extends TwseBeforeIFRSsDetail {
+      override val page: String = conf.getString("data.incomeStatement.page.afterIFRSs")
+      override val fileName = s"${super.endDate.getYear}_${super.endDate.getMonthValue}_a_"
+    }
+
+    class TpexBeforeIFRSsDetail extends TwseBeforeIFRSsDetail {
+      override val dir: String = conf.getString("data.incomeStatement.dir.tpex")
+
+      override def formData: Map[String, String] = super.formData + ("TYPEK" -> "otc")
+    }
+
+    class TpexAfterIFRSsDetail extends TpexBeforeIFRSsDetail {
+      override val page: String = conf.getString("data.incomeStatement.page.afterIFRSs")
+      override val fileName = s"${super.endDate.getYear}_${super.endDate.getMonthValue}_a_"
+    }
+
+    val twse: Detail = new TwseAfterIFRSsDetail
+    val tpex: Detail = new TpexAfterIFRSsDetail
+    val markets: Seq[Detail] = year match {
+      case y if y < 1993 => Seq(new TwseBeforeIFRSsDetail)
+      case y if y < 2013 => Seq(new TwseBeforeIFRSsDetail, new TpexBeforeIFRSsDetail)
+      case _ => Seq(new TwseAfterIFRSsDetail, new TpexAfterIFRSsDetail)
+    }
+  }
 
   case class ExRightDividendSetting(strDate: LocalDate = LocalDate.now, endDate: LocalDate = LocalDate.now) extends Setting {
     val twse: TwseDetail = new TwseDetail(LocalDate.of(2003, 5, 5), Some(strDate), endDate) {
@@ -214,7 +300,7 @@ object Settings {
   }
 
   case class MarginTransactionsSetting(date: LocalDate = LocalDate.now) extends Setting {
-    val twse: TwseDetail = new TwseDetail(LocalDate.of(2001, 1, 1), None, date) {
+    val twse: TwseDetail = new TwseDetail(LocalDate.of(2001, 1, 2), None, date) {
       val file: String = conf.getString("data.marginTransactions.twse.file")
       val dir: String = conf.getString("data.marginTransactions.twse.dir")
     }
