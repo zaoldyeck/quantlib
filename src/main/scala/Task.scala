a9db882d281ed6366ed8709d00c4c240ff13bddc
zaoldyeck <zaoldyeck9970@gmail.com>
2020-09-08 00:13:51 +0800

    Update Task

--- src/main/scala/Task.scala
+++ src/main/scala/Task.scala
@@ -7,6 +7,7 @@ import slick.jdbc.PostgresProfile.api._
 //import slick.jdbc.MySQLProfile.api._
 //import slick.jdbc.H2Profile.api._
 import slick.lifted.TableQuery
+import util.Helpers.SeqExtension
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
@@ -19,28 +20,34 @@ class Task {
 
   def createTables(): Unit = {
     val balanceSheet = TableQuery[BalanceSheet]
+    val conciseBalanceSheet = TableQuery[ConciseBalanceSheet]
     val capitalReduction = TableQuery[CapitalReduction]
     val dailyQuote = TableQuery[DailyQuote]
     val dailyTradingDetails = TableQuery[DailyTradingDetails]
     val exRightDividend = TableQuery[ExRightDividend]
     val financialAnalysis = TableQuery[FinancialAnalysis]
     val incomeStatementProgressive = TableQuery[IncomeStatementProgressive]
+    val conciseIncomeStatementProgressive = TableQuery[ConciseIncomeStatementProgressive]
+    val cashFlowsProgressive = TableQuery[CashFlowsProgressive]
     val index = TableQuery[Index]
     val marginTransactions = TableQuery[MarginTransactions]
     val operatingRevenue = TableQuery[OperatingRevenue]
     val stockPER_PBR_DividendYield = TableQuery[StockPER_PBR_DividendYield]
     val setup = DBIO.seq(
       balanceSheet.schema.create,
-      capitalReduction.schema.create,
-      dailyQuote.schema.create,
-      dailyTradingDetails.schema.create,
-      exRightDividend.schema.create,
-      financialAnalysis.schema.create,
+      //      conciseBalanceSheet.schema.create,
+      //      capitalReduction.schema.create,
+      //      dailyQuote.schema.create,
+      //      dailyTradingDetails.schema.create,
+      //      exRightDividend.schema.create,
+      //      financialAnalysis.schema.create,
+      //      conciseIncomeStatementProgressive.schema.create,
       incomeStatementProgressive.schema.create,
-      index.schema.create,
-      marginTransactions.schema.create,
-      operatingRevenue.schema.create,
-      stockPER_PBR_DividendYield.schema.create)
+      cashFlowsProgressive.schema.create)
+    //      index.schema.create,
+    //      marginTransactions.schema.create,
+    //      operatingRevenue.schema.create,
+    //      stockPER_PBR_DividendYield.schema.create)
 
     val db = Database.forConfig("db")
     try {
@@ -50,20 +57,13 @@ class Task {
   }
 
   def pullFinancialAnalysis(): Unit = {
-    val existFiles = FinancialAnalysisSetting().twse.dir.toDirectory.files.map {
-      file =>
-        val fileNamePattern = """(\d+)_.*.csv""".r
-        val fileNamePattern(year) = file.name
-        year.toInt
-    }.toSet
-
+    val existFiles = FinancialAnalysisSetting().twse.getYearsOfExistFiles
     val today = LocalDate.now()
     val thisYear = today.getYear
     val thisMonth = today.getMonthValue
     val lastYear = if (thisMonth > 3) thisYear - 1 else thisYear - 2
-    val futures = (1989 to lastYear).filterNot(existFiles).map(crawler.getFinancialAnalysis)
-
-    runFutures(futures)
+    val future = (1989 to lastYear).filterNot(existFiles).mapInSeries(crawler.getFinancialAnalysis)
+    Await.result(future, Duration.Inf)
   }
 
   def pullBalanceSheet(): Unit = {
@@ -74,29 +74,87 @@ class Task {
     pullQuarterlyFiles(IncomeStatementSetting().twse, crawler.getIncomeStatement)
   }
 
-  def pullOperatingRevenue(): Unit = {
-    val detail = OperatingRevenueSetting().twse
-    val existFiles = detail.dir.toDirectory.files.map {
-      file =>
-        val fileNamePattern = """(\d+)_(\d+).*""".r
-        val fileNamePattern(year, month) = file.name
-        (year.toInt, month.toInt)
-    }.toSet
+  def pullFinancialStatements(): Unit = {
+    val db = Database.forConfig("db")
+    val operatingRevenue = TableQuery[OperatingRevenue]
+    val detail = FinancialStatementsSetting().twse
+    val existFiles = detail.dir.toDirectory.dirs.map {
+      dir =>
+        val fileNamePattern = """(\d+)_(\d+)""".r
+        val fileNamePattern(y, q) = dir.name
+        val year = y.toInt
+        val quarter = q.toInt
+        year match {
+          case y if y < 2019 =>
+            dir.files.filter(f => year < 2019 || f.length > 10000).toSeq.map {
+              file =>
+                val fileNamePattern = """(\w+).*""".r
+                val fileNamePattern(companyCode) = file.name
+                (year, quarter, companyCode)
+            }
+          case _ => Seq((year, quarter, ""))
+        }
+    }.reduce(_ ++ _).toSet
 
-    val firstYear = detail.firstDate.getYear
+    val firstDate = detail.firstDate
+    val firstYear = firstDate.getYear
+    val thisYear = LocalDate.now.getYear
+    val firstYearToQuarter = (firstDate.getMonthValue to 4).map(quarter => (firstYear, quarter))
+    val yearToQuarter = for {
+      year <- firstYear + 1 until thisYear
+      quarter <- 1 to 4
+    } yield (year, quarter)
+    val thisYearToQuarter = LocalDate.now.getMonthValue match {
+      case m if m > 11 => (1 to 3).map(quarter => (thisYear, quarter))
+      case m if m > 8 => (1 to 2).map(quarter => (thisYear, quarter))
+      case m if m > 5 => Seq((thisYear, 1))
+      case _ => Seq()
+    }
+    val yearToQuarterToCompany = firstYearToQuarter.appendedAll(yearToQuarter).appendedAll(thisYearToQuarter).map {
+      case (year, quarter) =>
+        year match {
+          case y if y < 2019 =>
+            val filter = quarter match {
+              case 1 =>
+                operatingRevenue.filter(o => o.year === year && o.month < 4)
+              case 2 =>
+                operatingRevenue.filter(o => o.year === year && o.month > 3 && o.month < 7)
+              case 3 =>
+                operatingRevenue.filter(o => o.year === year && o.month > 6 && o.month < 10)
+              case 4 =>
+                operatingRevenue.filter(o => o.year === year && o.month > 9)
+            }
+            val companies = Await.result(db.run(filter.map(_.companyCode).distinct.result), Duration.Inf)
+            companies.map((year, quarter, _))
+          case _ => Seq((year, quarter, ""))
+        }
+    }.reduce(_ ++ _)
+    db.close()
+
+    val tuples = yearToQuarterToCompany.filterNot(existFiles)
+    val future = tuples.mapInSeries {
+      case (year, quarter, companyCode) => crawler.getFinancialStatements(year, quarter, companyCode)
+    }
+    Await.result(future, Duration.Inf)
+  }
+
+  def pullOperatingRevenue(): Unit = {
+    val setting = OperatingRevenueSetting()
+    val existFiles = setting.getTuplesOfExistFiles
+    val firstDate = setting.twse.firstDate
+    val firstYear = firstDate.getYear
     val thisYear = LocalDate.now.getYear
     val thisMonth = LocalDate.now.getMonthValue
-    val firstYearToMonth = (detail.firstDate.getMonthValue to 12).map(month => (firstYear, month))
+    val firstYearToMonth = (firstDate.getMonthValue to 12).map(month => (firstYear, month))
     val yearToMonth = for {
       year <- firstYear + 1 until thisYear
       month <- 1 to 12
     } yield (year, month)
     val thisYearToMonth = (1 to (if (LocalDate.now.getDayOfMonth > 10) thisMonth - 1 else thisMonth - 2)).map(month => (thisYear, month))
-
-    val futures = firstYearToMonth.appendedAll(yearToMonth).appendedAll(thisYearToMonth).filterNot(existFiles).map {
+    val future = firstYearToMonth.appendedAll(yearToMonth).appendedAll(thisYearToMonth).filterNot(existFiles).mapInSeries {
       case (year, month) => crawler.getOperatingRevenue(year, month)
     }
-    runFutures(futures)
+    Await.result(future, Duration.Inf)
   }
 
   def pullDailyQuote(): Unit = {
@@ -124,64 +182,35 @@ class Task {
   }
 
   def pullCapitalReduction(): Unit = {
-    val detail = CapitalReductionSetting().twse
-    val existFiles = detail.dir.toDirectory.files.toSeq.map {
-      file =>
-        val fileNamePattern = """(\d+)_(\d+)_(\d+).csv""".r
-        val fileNamePattern(year, month, day) = file.name
-        LocalDate.of(year.toInt, month.toInt, day.toInt)
-    }
+    val setting = CapitalReductionSetting()
+    val existFiles = setting.getDatesOfExistFiles
     val endDate = LocalDate.now.minusDays(1)
     if (existFiles.isEmpty) {
-      runFutures(Seq(crawler.getCapitalReduction(detail.firstDate, endDate)))
+      Await.result(crawler.getCapitalReduction(setting.twse.firstDate, endDate), Duration.Inf)
     } else if (existFiles.max != endDate) {
-      runFutures(Seq(crawler.getCapitalReduction(existFiles.max.plusDays(1), endDate)))
+      Await.result(crawler.getCapitalReduction(existFiles.max.plusDays(1), endDate), Duration.Inf)
     }
   }
 
   def pullExRightDividend(): Unit = {
-    val detail = ExRightDividendSetting().twse
-    val existFiles = detail.dir.toDirectory.files.toSeq.map {
-      file =>
-        val fileNamePattern = """(\d+)_(\d+)_(\d+).csv""".r
-        val fileNamePattern(year, month, day) = file.name
-        LocalDate.of(year.toInt, month.toInt, day.toInt)
-    }
+    val setting = ExRightDividendSetting()
+    val existFiles = setting.getDatesOfExistFiles
     val endDate = LocalDate.now.minusDays(1)
     if (existFiles.isEmpty) {
-      runFutures(Seq(crawler.getExRightDividend(detail.firstDate, endDate)))
+      Await.result(crawler.getExRightDividend(setting.twse.firstDate, endDate), Duration.Inf)
     } else if (existFiles.max != endDate) {
-      runFutures(Seq(crawler.getExRightDividend(existFiles.max.plusDays(1), endDate)))
+      Await.result(crawler.getExRightDividend(existFiles.max.plusDays(1), endDate), Duration.Inf)
     }
   }
 
   private def pullDailyFiles(detail: Detail, crawlerFunction: LocalDate => Future[Seq[File]]): Unit = {
-    val existFiles = detail.dir.toDirectory.files.flatMap {
-      file =>
-        val fileNamePattern = """(\d+)_(\d+)_(\d+).csv""".r
-        val fileNamePattern(year, month, day) = file.name
-        val y = year.toInt
-        val m = month.toInt
-        val d = day.toInt
-        val date = LocalDate.of(y, m, d)
-        val lines = file.lines("Big5-HKSCS")
-        val firstLineOption = lines.nextOption
-        //if ((firstLineOption.isEmpty && date.getDayOfWeek.getValue < 6) || (firstLineOption == Option("<html>"))) None else Some(date)
-        if (firstLineOption == Option("<html>")) None else Some(date)
-    }.toSet
-
-    val futures = detail.firstDate.datesUntil(LocalDate.now()).toScala(Seq).filterNot(existFiles).map(crawlerFunction)
-    runFutures(futures)
+    val existFiles = detail.getDatesOfExistFiles
+    val future = detail.firstDate.datesUntil(LocalDate.now()).toScala(Seq).filterNot(existFiles).mapInSeries(crawlerFunction)
+    Await.result(future, Duration.Inf)
   }
 
   private def pullQuarterlyFiles(detail: Detail, crawlerFunction: (Int, Int) => Future[Seq[File]]): Unit = {
-    val existFiles = detail.dir.toDirectory.files.map {
-      file =>
-        val fileNamePattern = """(\d+)_(\d+).*.csv""".r
-        val fileNamePattern(year, quarter) = file.name
-        (year.toInt, quarter.toInt)
-    }.toSet
-
+    val existFiles = detail.getTuplesOfExistFiles
     val thisYear = LocalDate.now.getYear
     val yearToQuarter = for {
       year <- detail.firstDate.getYear until thisYear
@@ -195,11 +224,9 @@ class Task {
       case _ => Seq()
     }
 
-    val futures = yearToQuarter.appendedAll(thisYearToQuarter).filterNot(existFiles).map {
+    val future = yearToQuarter.appendedAll(thisYearToQuarter).filterNot(existFiles).mapInSeries {
       case (year, quarter) => crawlerFunction(year, quarter)
     }
-    runFutures(futures)
+    Await.result(future, Duration.Inf)
   }
-
-  private def runFutures(futures: Seq[Future[Any]]): Unit = Await.result(Future.sequence(futures), Duration.Inf)
 }
